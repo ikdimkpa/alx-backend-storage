ce6512df5fa0adfd8a512c8b234018f074d23cd9
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-14 20:41:15 +0000

    debugged tasks 0 and 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- creates a table users
 
 CREATE TABLE IF NOT EXISTS users (
-	id int AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	PRIMARY KEY (id)
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
 );
