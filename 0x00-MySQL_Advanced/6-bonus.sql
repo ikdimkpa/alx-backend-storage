1d00a4ee87ee6aae8ae9d3e24579d30d9654574a
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-16 18:06:48 +0000

    Debugged task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -7,16 +7,12 @@ DELIMITER $$
 CREATE PROCEDURE AddBonus(
 		IN user_id INT,
 		IN project_name VARCHAR(255)
-	    IN score FLOAT)
+	    IN score INT)
 BEGIN
-	DECLARE project_id INT;
-	SELECT id INTO project_id FROM projects WHERE name = project_name;
-	
-	IF project_id IS NULL THEN
-		INSERT INTO projects (name) VALUES (project_name);
-		SET project_id = LAST_INSERT_ID();
-	END IF;
+	INSERT INTO projects (name)
+	SELECT project_name WHERE project_name NOT IN (SELECT name FROM projects);
 
-	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+	INSERT INTO corrections (user_id, project_id, score)
+   	VALUES (user_id, (SELECT id FROM projects WHERE name=project_name, score);
 END$$
 DELIMITER ;
