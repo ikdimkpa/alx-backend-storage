36a9b2971308817de9bc16fd8c18e40168c33521
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-16 17:40:48 +0000

    Tasks 11 and 12 completed!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,14 +1,13 @@
--- reates a stored procedure ComputeAverageWeightedScoreForUser that computes
--- and store the average weighted score for a student.
+-- reates a stored procedure ComputeAverageWeightedScoreForUsers that computes
+-- and store the average weighted score for all students.
 
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
 BEGIN
 	UPDATE users
 	SET average_score = SELECT (SUM(corrections.score * project.weight / SUM(projects.weight))
 	FROM corrections INNER JOIN project ON projects.id = corrections.project_id
-	WHERE corrections.user_id = user_id)
-	WHERE user.id = user_id;
+	WHERE corrections.user_id = user_id);
 END$$
 DELIMITER ;
