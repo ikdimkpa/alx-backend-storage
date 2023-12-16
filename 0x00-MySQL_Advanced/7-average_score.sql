f27234c31eb85e404cc425898d2c5e3562a30821
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-16 17:51:04 +0000

    Debugging task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -11,6 +11,6 @@ BEGIN
 	SET average_score = (SELECT AVG(score)
 	   					FROM corrections
 					   	WHERE corrections.user_id = user_id)
-				   		WHERE id = user_id;
+	WHERE id = user_id;
 END$$
 DELIMITER ;
