6f89c69d6b52ec0317199ca0e3d7c0ded54b88bd
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-22 17:39:49 +0000

    Debugging Task 15

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -11,38 +11,37 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.log.nginx
 
-    n_logs = nginxcollection.count_document({})
+    n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
     for method in methods:
-        count = nginx_collection.count_documents({"method": method}) 
-        print(f'\tmethod {method} {count}')
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
 
-    status_check = nginx_collection.count_document(
+    status_check = nginx_collection.count_documents(
             {"method": "GET", "path": "/status"}
     )
-    
     print(f'{status_check} status check')
-
     top_ips = nginx_collection.aggregate([
         {"$group":
-            {"_id": "$ip",
-             "count": {"$sum": 1}
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
             }
         },
-        {"$sort": {"count": 1}},
+        {"$sort": {"count": -1}},
         {"$limit": 10},
-        {"project": {
+        {"$project": {
             "_id": 0,
             "ip": "$_id",
             "count": 1
         }}
     ])
 
-    print('IPs:')
+    print("IPs:")
     for top_ip in top_ips:
-        ip = top_ip.get('ip')
-        count =top_ip.get('count')
-        print(f'\t {ip}: {count}')
+        ip = top_ip.get("ip")
+        count = top_ip.get("count")
+        print(f'\t{ip}: {count}')
