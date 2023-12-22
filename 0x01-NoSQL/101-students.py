c51cdaf528df89ccda7bbdd35622859f9d962d69
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-22 16:28:35 +0000

    Attempted Task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+   Creates a Python function that returns all students
+   sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    topStudent = mongo_collection.aggregate([
+            {
+                "projects": {
+                    "name": "$name",
+                    "average_score": {"$avg": "$topics.score"}
+                    }
+                },
+            {"$sort": "$avgerage_score": -1}
+    ])
