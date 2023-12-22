6a9d761a05dba50a795c59c5a05ddc66efb4f08b
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-22 17:26:19 +0000

    Attempted Task 15

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+   Improve 12-log_stats.py by adding the top 10 of the most present
+   IPs in the collection nginx of the database logs
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """Provides info on Nginx logs stored on MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.log.nginx
+
+    n_logs = nginxcollection.count_document({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method}) 
+        print(f'\tmethod {method} {count}')
+
+    status_check = nginx_collection.count_document(
+            {"method": "GET", "path": "/status"}
+    )
+    
+    print(f'{status_check} status check')
+
+    top_ips = nginx_collection.aggregate([
+        {"$group":
+            {"_id": "$ip",
+             "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": 1}},
+        {"$limit": 10},
+        {"project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+
+    print('IPs:')
+    for top_ip in top_ips:
+        ip = top_ip.get('ip')
+        count =top_ip.get('count')
+        print(f'\t {ip}: {count}')
