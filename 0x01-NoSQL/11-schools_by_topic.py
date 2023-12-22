10fa1d51c0c3a338393a96196fbe470fbdc321c6
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-12-22 14:41:09 +0000

    Debugging Task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,6 +7,6 @@ having a specific topic.
 
 def schools_by_topic(mongo_collection, topic):
     """Returns the list of school having a specific topic."""
-    docs = mongo_collection.find({"topic": topic})
+    docs = mongo_collection.find( {"topic": topic} )
     
     return list(docs)
